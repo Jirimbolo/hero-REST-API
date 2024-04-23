7ded78540c0604894112dcb68380a25c5f70d89e
Jiri Hyvönen <jirihyvonen@gmail.com>
2024-04-23 13:18:22 +0300

    Commit on main

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // Täällä alustetaan palvelin
-
+// const database = require('database');
+// const api = require('API');
 const http = require('http');
-const url = require("url");
-const database = require('database');
-const api = require('API');
 const PORT = 3000;
 
 
 const server = http.createServer((req, res) => {
-
-    const reqURL = url.parse(req.url).pathname;
-
-
-    //  res.writeHead(200, { 'content-type': 'text/plain' });
-    //  res.end("Hello world!");
+res.writeHead(200, { 'content-type': 'text/plain' });
+res.end("Hello world!");
 });
 server.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`)
