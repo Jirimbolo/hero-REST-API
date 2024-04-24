4df568b392fa44c6c4fa3c727bd0f755098a3f35
Jiri Hyvönen <jirihyvonen@gmail.com>
2024-04-24 13:11:35 +0300

    Päivitetty app

--- API.js
+++ API.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const Event = require('events');
-const api = express.Router();
+// const express = require('express');
+// const Event = require('events');
+// const router = express.Router();
 
 
 //luodaan seuraavanlaiset (CRUD) reitit
@@ -9,23 +9,5 @@ const api = express.Router();
 //POST
 //DELETE
 
-// GET ALL
-app.get('/', function (req, res) {
-    console.log("Get all aka READ");
-});
-// Get by ID
-app.get('/:id', function (req, res) {
-    console.log("Get by id aka READ");
-});
-// POST
-app.get('/create', function (req, res) {
-    console.log("Create new");
-});
-// PUT
-app.get('/update', function (req, res) {
-    console.log("Update");
-});
-// DELETE by ID
-app.get('/delete', function (req, res) {
-    console.log("Delete by ID");
-});
+
+// module.exports = router
--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-// Täällä alustetaan palvelin
+// Täällä alustetaan palvelin 
+// lataamalla moduuleja ja asettamalla polkuja
 
+const express = require('express');
+const app = express();
 // const database = require('database');
-// const api = require('API');
-const http = require('http');
-//const fs = require('fs');
-const PORT = 3000;
+// const routes = require('./api');
+
+// luodaan dotenv kansio ja palvelinportti
+const dotenv = require ('dotenv').config();
+const port = dotenv.parsed.PORT00 || 3000;
+
+// otetaan reitit käyttöön
+// app.use('./api', routes);
+// CRUD Reittejä GETall, GETid, POST, PUT, DELETE
+
+app.get('/', (req, res) => {
+res.send('Hello world');
+console.log("Hello world");
+});
+
+app.get('/api/all', (req, res) => {
+  res.send([1, 2, 3, 4]);
+  console.log("getAll");
+    });
+
+app.get('/api/:1', (req, res) => {
+ res.send(req.params.id);
+ console.log("getID")
+   });
+
+app.post('/api/post', (req, res) => {
+         res.send("POST");
+         console.log("post");
+          });
+
+app.put('/api/', (req, res) => {
+   res.send("PUT");  
+   console.log("update");  
+});
+
+ app.delete('/api/delete', (req, res) => {
+   res.send("Delete");
+   console.log("delete"); 
+});
 
 
-const server = http.createServer((req, res) => {
-res.writeHead(200, { 'content-type': 'text/plain' });
-res.end("Hello world!");
-});
-server.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
-});
+
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`)
+);
