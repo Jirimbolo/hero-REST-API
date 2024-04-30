df6dc1bcac043726aeac951efd6ffe96487772b7
Jiri Hyvönen <jirihyvonen@gmail.com>
2024-04-30 22:57:28 +0300

    api ja database päivitetty

--- database.js
+++ database.js
@@ -3,7 +3,5 @@
 // const mongoose = require('mongoose');
 // mongoose.connect(``);
 // const database = mongoose.connection;
-// database.on('error', console.error.bind(console, 'connection error:'));
-// database.once('open', function () {
-//  console.log('Database connected');
+// module.exports = mongoose
 // });
